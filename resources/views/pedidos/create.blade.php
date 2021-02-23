845f2277bb122f33e521269eab4481b7da627718
William Gerenutti <williamgerenuttidm@gmail.com>
2021-02-23 00:09:35 -0300

    Ajustes em Pedidos com Bootstrap

--- resources/views/pedidos/create.blade.php
+++ resources/views/pedidos/create.blade.php
@@ -1,43 +1,46 @@
 @extends('layouts.app')
-
 @section('content')
-    <p class="text-center fs-1">Fazer novo Pedido</p>
-    <div class="float-left">
-      <a class="btn btn-primary" href="{{ route('pedidos.index') }}" title="Voltar"> <i class="fas fa-backward "></i> </a>
-    </div></br> 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Ops!</strong> Há um problema com seu pedido<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <div class="row h-100">
-        <div class="col">
-            <form action="{{ route('pedidos.store') }}" method="POST">
-                    <div class="form-group col-xs-12 col-sm-12 col-md-7">
-                        <strong>Nome:</strong>
-                        <input type="text" name="nome" class="form-control" placeholder="João da Silva Alberto">
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-12 col-md-7">
-                            <strong>CPF:</strong>
-                            <input type="text" name="cpf" class="form-control" placeholder="111.111.111-11">
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-12 col-md-7">
-                            <strong>Telefone:</strong>
-                            <input type="number" name="telefone" class="form-control" placeholder="(33)3333-3333">
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-12 col-md-7">
-                            <strong>Endereço:</strong>
-                            <input type="text" name="endereco" class="form-control" placeholder="Rua Das Flores n95 Jardim Maracanã - Londrina - PR">
-                    </div></br>
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <button type="submit" class="btn btn-primary">Enviar</button>
-                    </div>
-            </form>
-        </div>
+<p class="text-center fs-1">Fazer novo Pedido</p>
+<div class="float-left">
+    <a class="btn btn-primary" href="{{ route('pedidos.index') }}" title="Voltar"> <i class="fas fa-backward "></i> </a>
+</div></br>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Ops!</strong> Há um problema com seu pedido<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+<div class="row h-100">
+    <div class="col">
+        <form action="{{ route('pedidos.store') }}" method="POST">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div></br>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </form>
     </div>
+</div>
 @endsection
--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1,58 +1,62 @@
 @extends('layouts.app')
-
 @section('content')
 </br>
+<div class="container content">
     <div class="row">
-        <div class="col-lg-12 margin-tb">
+        <div class="col-md ">
             <div class="pull-left">
                 <h2>Cadastro de Pedidos</h2>
             </div>
+            </br>
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+            @endif
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Nº</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Data de criação</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pedidos as $pedido)
+                    <tr>
+                        <td scope="row">>{{ $pedido->id }}</td>
+                        <td scope="row">></td>
+                        <td scope="row">>{{ $pedido->status }}</td>
+                        <td scope="row">>{{ date_format($pedido->created_at, 'd-m-Y H:i:s') }}</td>
+                        <td>
+                            <form action="{{ route('pedidos.destroy', $pedido->id) }}" method="POST">
+                                <a href="{{ route('pedidos.show', $pedido->id) }}" title="Visualizar">
+                                    <i class="fas fa-eye text-success fa-lg"></i>
+                                </a>
+                                <a href="{{ route('pedidos.edit', $pedido->id) }}" title="Editar">
+                                    <i class="fas fa-edit fa-lg"></i>
+                                </a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" title="Deletar" style="border: none; background-color:transparent;">
+                                    <i class="fas fa-trash fa-lg text-danger"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('pedidos.create') }}" title="Adicionar um Pedido"> <i class="fas fa-plus-circle"></i>
+                </a>
+            </div>
         </div>
     </div>
-    </br>
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
+</div>
 
-    <table class="table table-bordered table-responsive-lg">
-        <tr>
-            <th>Nº</th>
-            <th>Cliente</th>
-            <th>Status</th>
-            <th width="280px">Data de criação</th>
-            <th>Ação</th>
-        </tr>
-        @foreach ($pedidos as $pedido)
-            <tr>
-                <td>{{ $pedido->id }}</td>
-                <td></td>
-                <td>{{ $pedido->status }}</td>
-                <td>{{ date_format($pedido->created_at, 'd-m-Y H:i:s') }}</td>
-                <td>
-                    <form action="{{ route('pedidos.destroy', $pedido->id) }}" method="POST">
-                        <a href="{{ route('pedidos.show', $pedido->id) }}" title="Visualizar">
-                            <i class="fas fa-eye text-success fa-lg"></i>
-                        </a>
-                        <a href="{{ route('pedidos.edit', $pedido->id) }}" title="Editar">
-                            <i class="fas fa-edit fa-lg"></i>
-                        </a>
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" title="Deletar" style="border: none; background-color:transparent;">
-                            <i class="fas fa-trash fa-lg text-danger"></i>
-                        </button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-    <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('pedidos.create') }}" title="Adicionar um Pedido"> <i class="fas fa-plus-circle"></i>
-                    </a>
-            </div>
-
-    {!! $pedidos->links() !!}
+{!! $pedidos->links() !!}
 
 @endsection
