45792df7175b249763dcc205c98c6231e1698c5e
William Gerenutti Alves <william_gerenutti@hotmail.com>
2021-02-24 13:58:56 -0300

    Exibindo clientes no pedido.show

--- resources/views/pedidos/show.blade.php
+++ resources/views/pedidos/show.blade.php
@@ -2,50 +2,48 @@
 
 
 @section('content')
-    </br>
+</br>
+<div class="container content">
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> {{ $cliente->nome }}</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('clientes.index') }}" title="Voltar"> <i
-                        class="fas fa-backward "></i> </a>
+        <div class="col-md ">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2> Exibindo o pedido nº: {{ $pedido->id }}</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('pedidos.index') }}" title="Voltar"> <i class="fas fa-backward "></i> </a>
+                </div>
             </div>
         </div>
-    </div>
-    </br>
+        </br>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nome:</strong>
-                {{ $cliente->nome }}
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Pedido nº: {{ $pedido->id }}</strong>
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>cpf:</strong>
-                {{ $cliente->cpf }}
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Status:</strong>
+                    {{ $pedido->status }}
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>telefone:</strong>
-                {{ $cliente->telefone }}
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Cliente:</strong>
+                    @foreach ($pedido->clientes as $cliente)
+                    {{ $cliente->nome }}
+                    @endforeach
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Endereço:</strong>
-                {{ $cliente->endereco }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Data de criação:</strong>
-                {{ date_format($cliente->created_at, 'd-m-y') }}
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Data de criação:</strong>
+                    {{ date_format($pedido->created_at, 'd-m-y') }}
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
