a13805d2ba71ca749e9f2c6c2474f2151dc6d0b7
William Gerenutti <williamgerenuttidm@gmail.com>
2021-02-26 20:19:27 -0300

    Alterações no welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Crud básico NT</title>
+    <title>Crud NT</title>
     @stack('app.css')
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="utf-8">
